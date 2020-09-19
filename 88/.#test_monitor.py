87ddb6a15ad37a659ff684507c0644b5d892d5af
Forrest Smith <fasmith0229@gmail.com>
2020-09-19 17:36:17 -0500

    first stab

---
+++ 88/.#test_monitor.py
@@ -0,0 +1,1 @@
+forrest@darjeeling.local.81690
---
+++ 88/monitor.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from contextlib import contextmanager
+from datetime import date
+from time import time
+
+OPERATION_THRESHOLD_IN_SECONDS = 2.2
+ALERT_THRESHOLD = 3
+ALERT_MSG = 'ALERT: suffering performance hit today'
+
+violations = Counter()
+
+
+def get_today():
+    """Making it easier to test/mock"""
+    return date.today()
+
+
+@contextmanager
+def timeit() -> None:
+    start = time
+    yield
+    elapsed = time() - start
+    if elapsed > OPERATION_THRESHOLD_IN_SECONDS:
+        violations.update([elapsed])
+    if len(violations) > ALERT_THRESHOLD:
+        print(ALERT_MSG)
