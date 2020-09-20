ef598c0d0ec966146b2f7bbfda3dcf52940ae29a
Forrest Smith <fasmith0229@gmail.com>
2020-09-19 20:52:00 -0500

    first stab

---
+++ 152/decorator.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+
+DEFAULT_TEXT = ('Subscribe to our blog (sidebar) to periodically get '
+                'new PyBites Code Challenges (PCCs) in your inbox')
+DOT = '.'
+
+
+def strip_range(start, end):
+    """Decorator that replaces characters of a text by dots, from 'start'
+       (inclusive) to 'end' (exclusive) = like range.
+
+        So applying this decorator on a function like this and 'text'
+        being 'Hello world' it would convert it into 'Hel.. world' when
+        applied like this:
+
+        @strip_range(3, 5)
+        def gen_output(text):
+            return text
+    """
+    def decorator_replace(fun):
+        @wraps(fun)
+        def wrapper_replace(*args, **kwargs):
+            output = list(fun(*args, **kwargs))
+            for k in range(start, end):
+                output[k] = '.'
+            return ''.join(output)
+        return wrapper_replace
+    return decorator_replace
