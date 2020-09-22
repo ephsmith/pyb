3d001456e948e667e53d450b6950de02b20c6656
Forrest Smith <fasmith0229@gmail.com>
2020-09-22 17:07:42 -0500

    submission Bite 178 @ codechalleng.es

--- 178/commits.py
+++ 178/commits.py
@@ -1,7 +1,7 @@
 from collections import Counter
+import re
 import os
 from urllib.request import urlretrieve
-
 from dateutil.parser import parse
 
 commits = os.path.join(os.getenv("TMP", "/tmp"), 'commits')
@@ -12,6 +12,19 @@ urlretrieve(
 
 # you can use this constant as key to the yyyymm:count dict
 YEAR_MONTH = '{y}-{m:02d}'
+
+
+def _parse_line(line: str) -> dict:
+    """returns a line with the key of date type and value of add/del"""
+    d_str, all_changes = line.split(' | ')
+    date = parse(re.sub(r'Date:[ ]+', '', d_str)).date()
+
+    # add insertions and deletions
+    insertions = re.findall(r'([0-9]+) insertions', all_changes)
+    deletions = re.findall(r'([0-9]+) deletions', all_changes)
+    changes = int(insertions[0]) if insertions else 0
+    changes += int(deletions[0]) if deletions else 0
+    return {'date': date, 'changes': changes}
 
 
 def get_min_max_amount_of_commits(commit_log: str = commits,
@@ -25,4 +38,14 @@ def get_min_max_amount_of_commits(commit_log: str = commits,
 
     Returns a tuple of (least_active_month, most_active_month)
     """
-    pass
+    commits = Counter()
+    with open(commit_log) as f:
+        for line in f:
+            dat = _parse_line(line)
+            if dat['date'].year == year or year is None:
+                commits.update({dat['date'].strftime('%Y-%m'): dat['changes']})
+
+    max = commits.most_common(1)[0][0]
+    min = commits.most_common()[-1][0]
+
+    return min, max
