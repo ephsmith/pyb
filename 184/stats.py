704b985b46bef2200b664b306786103bba9e7921
Forrest Smith <fasmith0229@gmail.com>
2020-09-22 17:55:39 -0500

    submission Bite 184 @ codechalleng.es

--- 184/stats.py
+++ 184/stats.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 import os
 from urllib.request import urlretrieve
+from collections import Counter
 
 TMP = os.getenv("TMP", "/tmp")
 LOGS = 'bite_output_log.txt'
@@ -33,21 +34,25 @@ class BiteStats:
     @property
     def number_users_active(self) -> int:
         """Get the number of unique users in the data set"""
-        pass
+        return len({bite['user'] for bite in self.rows})
 
     @property
     def number_users_solving_bites(self) -> int:
         """Get the number of unique users that resolved
            one or more Bites"""
-        pass
+        return len({bite['user'] for bite in self.rows
+                    if bite['completed'] == 'True'})
 
     @property
     def top_bite_by_number_of_clicks(self) -> str:
         """Get the Bite that got accessed the most
            (= in most rows)"""
-        pass
+        counts = Counter([bite['bite'] for bite in self.rows])
+        return counts.most_common(1)[0][0]
 
     @property
     def top_user_by_bites_completed(self) -> str:
         """Get the user that completed the most Bites"""
-        pass
+        counts = Counter([bite['user'] for bite in self.rows
+                         if bite['completed'] == 'True'])
+        return counts.most_common(1)[0][0]
