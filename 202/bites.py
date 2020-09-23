44b12fb0559cf43df8ee496937ca890e18a4a1c1
Forrest Smith <fasmith0229@gmail.com>
2020-09-22 19:01:43 -0500

    submission Bite 202 @ codechalleng.es

--- 202/bites.py
+++ 202/bites.py
@@ -11,13 +11,25 @@ if not stats.exists():
     urlretrieve(data, stats)
 
 
+def _bitenum(bite_str):
+    return bite_str.split('.')[0].split(' ')[1]
+
+
 def get_most_complex_bites(N=10, stats=stats):
     """Parse the bites.csv file (= stats variable passed in), see example
        output in the Bite description.
        Return a list of Bite IDs (int or str values are fine) of the N
        most complex Bites.
     """
-    pass
+    with open(stats, encoding="utf-8-sig") as f:
+        bites = list(csv.reader(f, delimiter=';'))
+    bites.pop(0)  # remove header
+    bites = [[b[0], float(b[1])] for b in bites if b[1] != 'None']
+    bites.sort(key=lambda x: x[1], reverse=True)
+
+    n_most_comp = bites[:N]
+
+    return [_bitenum(b[0]) for b in n_most_comp]
 
 
 if __name__ == '__main__':
